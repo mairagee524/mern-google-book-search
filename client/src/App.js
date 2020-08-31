63754a91a63c63bdfb4a9b0f714ce2505f142355
mairagee524 <garciamaira524@yahoo.com>
2020-08-30 21:37:29 -0700

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+
+import Navbar from "./components/Navbar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

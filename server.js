b7dda6d43c9fecfc0cc211905a2aa5717227143c
mairagee524 <garciamaira524@yahoo.com>
2020-09-01 09:48:54 -0700

    changed folder structure and added code to server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const googleBookRouter = require('./routes/api-routes')
 const app = express();
 const PORT = 3000 || process.env.PORT; 
 
+if (app.get('env') === 'development') { require('dotenv').config(); }
+
+
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,6 +22,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
+// Add routes
 app.use('/api', googleBookRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

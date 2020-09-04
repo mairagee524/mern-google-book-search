09e94c3113c2fbfefa2c59e15c5f170b9d6a5402
mairagee524 <garciamaira524@yahoo.com>
2020-09-03 23:24:32 -0700

    removed /build from .gitignore

---
+++ client/build/precache-manifest.cd49d42465fef8a7b66a22f3b4babf8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb4c563319159d2e1e52351531da763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eaf62e1e3497a221fda",
+    "url": "/static/css/main.7c95773e.chunk.css"
+  },
+  {
+    "revision": "2e434b715cf724d27e74",
+    "url": "/static/js/2.6c3fbdae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6c3fbdae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eaf62e1e3497a221fda",
+    "url": "/static/js/main.f3850996.chunk.js"
+  },
+  {
+    "revision": "c9b6944a8b9e7435fea8",
+    "url": "/static/js/runtime-main.123f1695.js"
+  },
+  {
+    "revision": "b8548ae7f4e25e5ee2f5bf511f1ad649",
+    "url": "/static/media/library.b8548ae7.png"
+  }
+]);
